e32f64c51381c22019db26c51036efdad0cd88bc
jeffreylloydbrown <jeff_brown2@msn.com>
2018-01-19 13:32:34 -0600

    getPerimeter() and testPerimeter()
    Supplied the code provided in the videos to fill in these methods.

--- perimeter_quiz/PerimeterAssignmentRunner.java
+++ perimeter_quiz/PerimeterAssignmentRunner.java
@@ -8,7 +8,21 @@ public class PerimeterAssignmentRunner {
     }
     
     public double getPerimeter (Shape s) {
-        return 0.0;
+        // Start with totalPerim = 0
+        double totalPerim = 0;
+        // Start with prevPt = the last point
+        Point prevPt = s.getLastPoint();
+        // For each point currPt in the shape,
+        for (Point currPt : s.getPoints()) {
+            // Find distance from prevPt to currPt, name it currDist
+            double currDist = prevPt.distance(currPt);
+            // Update totalPerim to be totalPerim + currDist
+            totalPerim = totalPerim + currDist;
+            // Update prevPt to be currPt.
+            prevPt = currPt;
+        }
+        // totalPerim is my answer
+        return totalPerim;
     }
 
     public double getAverageLength(Shape s) {
@@ -38,6 +52,10 @@ public class PerimeterAssignmentRunner {
     }
     
     public void testPerimeter() {
+        FileResource fr = new FileResource();
+        Shape s = new Shape(fr);
+        double length = getPerimeter(s);
+        System.out.println("perimeter = " + length);
     }
 
     public void testPerimeterMultipleFiles() {
