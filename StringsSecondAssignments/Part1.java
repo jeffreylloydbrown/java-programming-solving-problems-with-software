95dcf2d089ab3592f0611055a6574806d51eb382
jeffreylloydbrown <jeff_brown2@msn.com>
2018-01-23 16:42:16 -0600

    findStopCodon(), testFindStopCodon(), first testFindGene() cases

--- StringsSecondAssignments/Part1.java
+++ StringsSecondAssignments/Part1.java
@@ -19,16 +19,59 @@ public class Part1 {
      * @param stopCodon    the codon to search for.
      * @returns the index of the first occurrence of `stopCodon`
      * past `startIndex` that is a multiple of 3 away from `startCodon`.
-     * If not found, return dna.length().
+     * If not found, return dna.length() (which works well with searching
+     * for a minimum value, as dna.length() is always greater than any
+     * possible stop codon index.)
      */
     public int findStopCodon (String dna, int startIndex, String stopCodon) {
+        // Check parameters.  If any are not valid, return "not found"
+        // (which is dna.length()).
+        if (dna.isEmpty() || stopCodon.isEmpty() || startIndex < 0 || startIndex >= dna.length()) {
+            return dna.length();
+        }
+        
+        // Keep searching for stop codons until none found.  When
+        // one is found, if it is a multiple of 3 away from startIndex
+        // return that index.  If not, keep searching at the next letter.
+        int currIndex = dna.indexOf(stopCodon, startIndex+3);
+        while (currIndex != -1) {
+            if ((currIndex - startIndex) % 3 == 0) { // found
+                return currIndex;
+            } else {
+                currIndex = dna.indexOf(stopCodon, currIndex+1);
+            }
+        }
         
         // Not found, so return the dna's length
         return dna.length();
     }
     
+    private void testFSC(String desc, String dna, int startIndex, String stopCodon, int expected) {
+        int index = findStopCodon(dna, startIndex, stopCodon);
+        if (index == expected) {
+            System.out.println("PASS: "+desc+": dna='"+dna+"'");
+        } else {
+            System.out.println("FAIL: "+desc+": dna='"+dna+"' expected "+expected+", got "+index);
+        }
+    }
+    
     /** Test driver for findStopCodon(). */
     public void testFindStopCodon () {
+        String dna = "xxxyyyzzzTAAxxxyyyzzzTAAxx";
+        testFSC("test 1", dna, 0, "TAA", 9);
+        testFSC("test 2", dna, 9, "TAA", 21);
+        testFSC("test 3", dna, 1, "TAA", dna.length());
+        testFSC("test 4", dna, 0, "TAG", dna.length());
+        testFSC("empty dna", "", 0, "TAG", 0);
+        testFSC("empty stop codon", dna, 0, "", dna.length());
+        testFSC("negative start index", dna, -1, "TAA", dna.length());
+        testFSC("start index too large", dna, dna.length(), "TAA", dna.length());
+        testFSC("smallest gene", "ATGTAA", 0, "TAA", 3);
+        testFSC("2 TAA, one not in right place", "ATGCTAACCTAA", 0, "TAA", 9);
+        testFSC("multiple stop codons TAG", "ATGTAGTGATAA", 0, "TAG", 3);
+        testFSC("multiple stop codons TGA", "ATGTAGTGATAA", 0, "TGA", 6);
+        testFSC("multiple stop codons TAA", "ATGTAGTGATAA", 0, "TAA", 9);
+        System.out.println("tests completed");
     }
     
     /** Locates the next valid gene sequence in `dna` and returns it.
@@ -41,8 +84,20 @@ public class Part1 {
         return "";
     }
     
+    private void testFG(String desc, String dna, String expected) {
+       String gene = findGene(dna);
+        if (gene.equals(expected)) {
+            System.out.println("PASS: "+desc+": dna='"+dna+"'");
+        } else {
+            System.out.println("FAIL: "+desc+": dna='"+dna+"' expected '"+expected+"', got '"+gene+"'");
+        }
+    }
     /** Test driver for findGene() */
     public void testFindGene() {
+        testFG("smallest gene", "ATGTAA", "ATGTAA");
+        testFG("2 TAA, one not in right place", "ATGCTAACCTAA", "ATGCTAACCTAA");
+        testFG("multiple stop codons TGA", "ATGTAGTGATAA", "ATGTAG");
+        System.out.println("tests completed");
     }
     
     /** Given a DNA strand `dna`, print each gene it contains to
