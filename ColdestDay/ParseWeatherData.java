c457236fae6ab46f976d32be606723f2fd8cb064
Brown <jeffrey.l.brown@thomsonreuters.com>
2018-02-08 17:25:23 -0600

    Implemented fileWithColdestTemperature() and test driver
    Since we don't know exception handling, the test expects user to pick 2014 temperature data---we have to hardcode the directory path until we can switch to getCanonicalPath().

--- ColdestDay/ParseWeatherData.java
+++ ColdestDay/ParseWeatherData.java
@@ -1,5 +1,6 @@
 import edu.duke.*;
 import org.apache.commons.csv.*;
+import java.io.*;
 
 /**
  * Find the coldest day of the year and other interesting facts about the temperature and humidity in a day.
@@ -12,6 +13,7 @@ public class ParseWeatherData {
     private String TEMPERATURE_COLUMN = "TemperatureF";
     private String TIME_COLUMN = "TimeEST";
     private String HUMIDITY_COLUMN = "Humidity";
+    private String DATE_COLUMN = "DateUTC";
     
     /** Find the CSVRecord with the coldest temperature in the data and thus all the information about 
      *  the coldest temperature, such as the hour of the coldest temperature.
@@ -36,14 +38,62 @@ public class ParseWeatherData {
         return coldestSoFar;
     }
     
+    /** Test driver for coldestHourInFile() */
     public void testColdestHourInFile () {
         FileResource fr = new FileResource("nc_weather/2014/weather-2014-01-08.csv");
         CSVRecord coldestHour = coldestHourInFile(fr.getCSVParser());
         if (coldestHour != null) {
             System.out.println("coldest temp is " + coldestHour.get(TEMPERATURE_COLUMN) + 
-                           " at time " + coldestHour.get(TIME_COLUMN));
+                               " at time " + coldestHour.get(TIME_COLUMN));
         } else {
             System.out.println("no valid temperature found");
+        }
+    }
+    
+    /** From a group of files the user picks, find the name of the file containing the coldest 
+     *  temperature value and return that name.
+     *  
+     *  @return the filename of the weather data file that contains the coldest temperature in a group of files.
+     */
+    public String fileWithColdestTemperature () {
+        DirectoryResource dr = new DirectoryResource();
+        String coldestFilename = null;
+        CSVRecord coldestSoFar = null;
+        
+        for (File f : dr.selectedFiles()) {
+            FileResource fr = new FileResource(f);
+            CSVRecord currentColdest = coldestHourInFile(fr.getCSVParser());
+            if (coldestFilename == null || coldestSoFar == null) {
+                coldestFilename = f.getName();  // getCanonicalPath() requires an exception handler.
+                coldestSoFar = currentColdest;
+            } else {
+                // Need temps to compare.
+                double coldestTemp = Double.parseDouble(coldestSoFar.get(TEMPERATURE_COLUMN));
+                double currentTemp = Double.parseDouble(currentColdest.get(TEMPERATURE_COLUMN));
+                if (currentTemp != -9999 && currentTemp < coldestTemp) {
+                    coldestFilename = f.getName();
+                    coldestSoFar = currentColdest;
+                }
+            }
+        }
+        
+        return coldestFilename;
+    }
+    
+    /** Test driver for fileWithColdestTemperature() */
+    public void testFileWithColdestTemperature () {
+        String coldestFilename = fileWithColdestTemperature();
+        
+        // using getCanonicalPath() in fileWithColdestTemperature() requires an exception handler,
+        // which we don't know yet.  So this test is kludged to hardcode choices in the 2014 directory.
+        FileResource fr = new FileResource("nc_weather/2014/"+coldestFilename);
+        CSVRecord coldestHour = coldestHourInFile(fr.getCSVParser());
+        
+        System.out.println("Coldest day was in file " + coldestFilename);
+        System.out.println("Coldest temperature on that day was " + coldestHour.get(TEMPERATURE_COLUMN));
+        System.out.println("All the Temperatures on the coldest day were:");
+        for (CSVRecord row : fr.getCSVParser()) {
+            System.out.println(row.get(DATE_COLUMN) + ": " + row.get(TEMPERATURE_COLUMN));
         }
     }
 
