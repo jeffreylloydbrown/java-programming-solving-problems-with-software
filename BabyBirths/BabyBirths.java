565cd70cb9a88e1ad43171fcad12a139868d3170
Brown <jeffrey.l.brown@thomsonreuters.com>
2018-02-14 17:08:08 -0600

    Added code from course videos, just in case I need it later.
    Will remove any unused code at the end of the assignment.

--- BabyBirths/BabyBirths.java
+++ BabyBirths/BabyBirths.java
@@ -29,57 +29,113 @@ import org.apache.commons.csv.*;
  * 
  */
 public class BabyBirths {
-    
+
     //////////////////////////////////////////////////////////////////////////////////
     //// Data representation.  All of this really should be put in its own class. ////
     //////////////////////////////////////////////////////////////////////////////////
-    
+
     // Columns are not named, so use variables to hold their current indices.
     // Avoids hard-coding so that if the layout changes later, just adjust it here.
     private int NAME = 0;
     private int GENDER = 1;
     private int COUNT = 2;
-    
+
     // Gender is encoded as a string, so it could change or get additional
     // values "later".  Use these constants instead of hard-coding.
     private String MALE = "M";
     private String FEMALE = "F";
-    
+
     // Accessors to help code readability, instead of more cryptic get() calls.
     private String getName (CSVRecord record) { return record.get(NAME); }
-    
+
     private String getGender (CSVRecord record) { return record.get(GENDER); }
-    
+
     // We don't technically know about try-catch, and I just don't care.  This avoids
     // convoluted error handling.  Since actual counts are not negative, return -1
     // for any error.  
     private int getCount (CSVRecord record) {
         int count = -1;
-        
+
         try {
             count = Integer.parseInt(record.get(COUNT));
         } catch (Exception e) {
             count = -1;
         }
-        
+
         return count;
     }
-    
+
     // We will need to test for gender, so encapsulate those.
     private boolean isFemale (CSVRecord record) { return getGender(record).equals(FEMALE); }
-    
+
     private boolean isMale (CSVRecord record) { return getGender(record).equals(MALE); }
-    
-    
+
     //////////////////////////////////////////////////////////////////////////////////
     //// Primary implementation.                                                  ////
     //////////////////////////////////////////////////////////////////////////////////
+
+    /** Example code from course video that reads a data file and does nothing interesting.
+     * 
+     *  @param year     the year to read
+     */
+    public void readOneFile (int year) {
+        String filename = "us_babynames_by_year/yob" + year + ".csv";
+        FileResource fr = new FileResource(filename);
+        for (CSVRecord rec : fr.getCSVParser(false)) {
+            String name = getName(rec);
+            String gender = getGender(rec);
+            // TODO
+        }
+    }
+
+    /** Example code from course video that prints each row in the data file the user selects. */
+    public void printNames () {
+        FileResource fr = new FileResource();
+        for (CSVRecord rec : fr.getCSVParser(false)) {
+            int numBorn = getCount(rec);
+            if (numBorn <= 100) {
+                System.out.println("Name " + getName(rec) + 
+                    " Gender " + getGender(rec) +
+                    " Num Born " + getCount(rec));
+            }
+        }
+    }
+
+    /** Example code from course video that calculates the total births as well as the girl births
+     *  and boy births and prints them.
+     *  
+     *  @param fr   A `FileResource` representing the CSV file to examine.
+     */
+    public void totalBirths (FileResource fr) {
+        int totalBirths = 0;
+        int totalBoys = 0;
+        int totalGirls = 0;
+        for (CSVRecord rec : fr.getCSVParser(false)) {
+            int numBorn = getCount(rec);
+            totalBirths += numBorn;
+            if (isMale(rec)) totalBoys += numBorn;
+            if (isFemale(rec)) totalGirls += numBorn;
+        }
+        System.out.println("total births = " + totalBirths);
+        System.out.println("total girls = " + totalGirls);
+        System.out.println("total boys = " + totalBoys);
+    }
     
-    
+    /** Test driver for totalBirths(), from the course video. */
+    public void testTotalBirths () {
+        FileResource fr = new FileResource("us_babynames_test/example-small.csv");
+        System.out.println("Expect 1700 total births, 1500 females, 200 males");
+        totalBirths(fr);
+        
+        fr = new FileResource("us_babynames_by_year/yob2014.csv");
+        System.out.println("Expect 3670151 total births, 1768775 females, 1901376 males");
+        totalBirths(fr);
+    }
+
     //////////////////////////////////////////////////////////////////////////////////
     //// Higher level tests.                                                      ////
     //////////////////////////////////////////////////////////////////////////////////
-    
+
     // These answers come from the course videos, which will become test cases.
     // 
     // Jennifer in 1994 is rank 21.
