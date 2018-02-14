e97ecc59c83a7c9f9606f7096e560a5e9cc22944
Brown <jeffrey.l.brown@thomsonreuters.com>
2018-02-14 16:13:05 -0600

    Initial setup for BabyBirths course project

---
+++ BabyBirths/BabyBirths.java
@@ -0,0 +1,13 @@
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+/**
+ * Given a large amount of data on babies born in the USA 1880-2014,
+ * answer different questions about all that data.
+ * 
+ * @author Jeff Brown
+ * @version 1
+ */
+public class BabyBirths {
+
+}  // BabyBirths
